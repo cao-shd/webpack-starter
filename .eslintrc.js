9612e519e1de8915860c1e16ae26166abb122f4d
cao-shd <cao-shd@outlook.com>
2020-06-14 12:39:38 +0800

    remove airbnb eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,15 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2020: true,
-    node: true,
+  "env": {
+    "browser": true,
+    "es2020": true,
+    "node": true,
   },
-  extends: 'airbnb',
-  parserOptions: {
-    ecmaVersion: 11,
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "ecmaVersion": 11,
+    "sourceType": "module",
   },
-  rules: {
+  "rules": {
     semi: 1, // 代码必须以分号结束
     'comma-dangle': [1, 'always-multiline'], // 对象或数组多行写法时，最后一个值加逗号
   },
